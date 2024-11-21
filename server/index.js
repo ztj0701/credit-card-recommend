4ab6cdc4e3332df2b2ad65a6c15742df2b923524
ztj0701 <131656510+ztj0701@users.noreply.github.com>
2024-11-21 23:29:16 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -139,6 +139,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
