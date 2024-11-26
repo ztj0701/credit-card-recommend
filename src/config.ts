3ced905eb9f8925390dd4fd70fddcc6a39d9a5ff
ztj0701 <131656510+ztj0701@users.noreply.github.com>
2024-11-26 23:21:28 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // In development and production, API calls will be made to the same origin
-const API_URL = 'https://credit-card-recommend-c63f8a90ea74.herokuapp.com/';  // Empty string means same origin
+const API_URL = 'https://credit-card-recommend-c63f8a90ea74.herokuapp.com';  // Empty string means same origin
 export { API_URL };
